fbd94edd90d124a27179eeadff3ea4127fd3294f
Vincent Caracciolo <vincentncaracciolo@gmail.com>
2019-09-24 07:07:55 -0400

    form submit working in console, time not displaying correctly

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function () {
 
 var firebaseConfig = {
     apiKey: "AIzaSyD5uUDQcClcOAPUR_5c0wnblbYLj7LJdYk",
@@ -8,14 +8,58 @@ var firebaseConfig = {
     storageBucket: "",
     messagingSenderId: "1073805895293",
     appId: "1:1073805895293:web:bf0079c158781ec5c77314"
-  };
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  $("#add-train-btn").on("click", function (event) {
+$("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
-  });
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var firstTrain = moment($("#first-input").val().trim(), "HH:mm").format("X");
+    var trainFrequency = $("#min-input").val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: firstTrain,
+        frequency: trainFrequency
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
+
+    alert("Train successfully added");
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-input").val("");
+    $("#min-input").val("");
+
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var firstTrain = childSnapshot.val().time;
+    var trainFrequency = childSnapshot.val().frequency;
+  
+    // Employee Info
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(firstTrain);
+    console.log(trainFrequency);
+
+});
+
+});
